2c1cb002980dc669f9aeaf0e60d3058532e1386a
xing24xing <khushipandey260304@gmail.com>
2025-04-24 02:09:21 +0530

    Fixed Btn Related Issues

--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import images from "../constant/Icon";
+import Button from './Button';
 function Logo() {
   return (
     <div className="w-full flex items-center justify-center bg-gradient-to-r from-slate-500 to-slate-950 text-white h-16 shadow-md">
             <h1 className="text-xl font-bold font-mono">
-            <BackButton icon={images.btn} label="Go Back" />
+            <Button icon={images.btn} label="Go Back" />
               Logo</h1>
     </div>
   )
