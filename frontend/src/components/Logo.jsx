e7e867c91c4983793e315ded96291cc1cb324ccd
xing24xing <khushipandey260304@gmail.com>
2025-04-24 02:23:42 +0530

    Fixed The btn issues 2

--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
-import images from "../constant/Icon";
-import Button from './Button';
+
 function Logo() {
   return (
     <div className="w-full flex items-center justify-center bg-gradient-to-r from-slate-500 to-slate-950 text-white h-16 shadow-md">
-            <h1 className="text-xl font-bold font-mono">
-
-              Logo</h1>
+            <h1 className="text-xl font-bold font-mono">Logo</h1>
     </div>
   )
 }
